926aa0b7200ec21b3da4901227a36e9f605a5838
NathanWang <nathanwang201@hotmail.com>
2018-06-24 18:31:23 +0930

    XML configuration for external bean

--- src/main/java/com/hao/jokes/JokesApplication.java
+++ src/main/java/com/hao/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package com.hao.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:joke-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
